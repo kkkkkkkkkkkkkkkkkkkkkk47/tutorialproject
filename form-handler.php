a65e045d8de0e6aedbeb7f56419ec4826fb21f2f
kkkkkkkkkkkkkkkkkkkkkk47 <koskanasa03@gmail.com>
2025-06-11 22:02:45 +0100

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'abiolakolade68@gmail.com';
+$email_form = 'tutorialproject.pages.dev/contact';
 
 $email_subject = 'New Form Submission';
 
